8cf0d5ba75fa378339123b909189c1c2acd4829c
Divya Prakash <divya.prakash@thoughts2binary.com>
2021-11-28 22:00:59 +0530

    User app for user account and apis

--- lovemint_django_backend/settings.py
+++ lovemint_django_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#*^go^d9k6ebrljb&_ivp1*f$9it_q)_l_kj$&=5bzl$5#v1i-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
 ]
 
 MIDDLEWARE = [
